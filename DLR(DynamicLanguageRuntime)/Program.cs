88e83596ac41fc6e39541e064232d1959291cbae
denizduman1 <ce.denizduman@gmail.com>
2020-10-31 17:59:01 +0300

    add

---
+++ DLR(DynamicLanguageRuntime)/Program.cs
@@ -0,0 +1,43 @@
+﻿using IronPython.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLR_DynamicLanguageRuntime_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DLR dynamic keyword aracığıyla farklı dillerin kodunu çalıştırabiliyoruz.
+            //Örnek olarak Python kodu çalıştıralacak..
+
+            //Python kodumuzun oluşturulma yerini çekiyoruz.
+            //Python kodunu burda da yazabilirdik.
+            var source = @"C:\Users\Deniz\Desktop\python\python_Examples\6.py";
+
+            //Python motorunu çalıştırıyoruz.
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            var oper = engine.Operations;
+
+            // uygulamayı çalıştırıyoruz.
+            engine.ExecuteFile(source,scope);
+
+            //Hesap sınıfını C# daki Hesap değişkenine attık.
+            //Hesap sınıfından ins adında nesne oluşturduk.
+            //dynamic tür olduğu için kod da olmayan fonksiyon (aslında python da olan)
+            //dinamik olarak çalışıyor.
+
+            var Hesap = scope.GetVariable("Hesap");
+            dynamic ins = oper.CreateInstance(Hesap);
+
+            Console.WriteLine((ins.topla(15,20)));
+            Console.WriteLine((ins.fark(20,15)));
+
+            Console.ReadLine();
+        }
+    }
+}
--- var_object_dynamic_fark/Program.cs
+++ var_object_dynamic_fark/Program.cs
@@ -10,6 +10,39 @@ namespace var_object_dynamic_fark
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            string y = "merhaba";
+
+            // gelen verinin ne olduğu bilinmediği (int mi string mi) durumlarda 'var' kullanılır
+            // var atanan değer türüne bürünür.
+            var z = "selam";
+            var w = 10;
+
+            object a = 5; //boxing işlemi yapılmış olunuyor.
+            object b = "selam";
+
+            Console.WriteLine("object a={0} and object b={1}", a, b);
+           
+            //unboxing(kutudan çıkarma) işlemi yapılarak başka değişkene atanabilir. 
+            //gelen verinin ne olduğu kestirilemediği durumda kutulama yapılıp 
+            //daha sonra istenilen şekilde kutu açılımı yapılmasına yarıyor.
+            int a1 = (int)a; 
+            string b1 = (string)b;
+
+
+            //object'deki gibi istenilen değişken tipini alabiliyor.
+            //object'ten farklı olarak başka değişkene atanırken casting işlemi yapmaya gerek yoktur.
+             
+            dynamic X = 5;
+            dynamic Y = "selam";
+            int X1 = X; 
+            string Y1 = Y;
+
+            // dynamic object'ten farklı olarak dinamik olarak çalışabilir aşağıdaki işlemi
+            // object'te çalıştırıyor olsaydık hata alacaktık.(unboxing yapmadan)
+            Console.WriteLine(Y.Length);
+
+            Console.ReadLine();
         }
     }
 }
