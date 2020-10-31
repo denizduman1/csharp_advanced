0ca490e7c206b286bfdb62966b5385de001bd389
denizduman1 <ce.denizduman@gmail.com>
2020-10-31 17:12:25 +0300

    Add project files.

---
+++ var_object_dynamic_fark/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var_object_dynamic_fark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
