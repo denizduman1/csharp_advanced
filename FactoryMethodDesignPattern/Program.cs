88f0da513b17b69d402d933236ad000c9cdd7269
denizduman1 <ce.denizduman@gmail.com>
2020-11-08 00:22:27 +0300

    add

---
+++ FactoryMethodDesignPattern/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Factory Method Design
+            Business b1 = new Business();
+            b1.Save();
+        }
+    }
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new DdLogger();
+        }
+    }
+    public interface ILoggerFactory
+    {
+
+    }
+    public interface ILogger
+    {
+        void Log();
+    }
+    public class DdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with DD");
+        }
+    }
+    public class Business
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = new LoggerFactory().CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton design patterni 
+            //Aynı işlemin birden fazla işlemciden çalıştırılıp her seferinde newlenmesi ram için pahalı bir işlemdir.
+            //Bu yüzden bir classtan tek bir örnek çıkarırız.
+            //Kullanıcıların bağlantı işleri gibi kendilerine özel olmayan işlevlerde bu patterni kullanırız
+            //Her kullanıcının kendisine ait yaptığı işlemlerde kullanmaya çalışırsak veriler karışır.
+
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+
+            var dataAccesObject = DataAccess.CreateInstance();
+            dataAccesObject.Display();
+
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private static object _lockObject = new object(); //aynı anda çalışabilecek threadler için kilit önlemi
+
+        private CustomerManager() { } //Dış erişimi private ile engelledik.
+        public static CustomerManager CreateAsSingleton() 
+        {
+            if (_customerManager==null)
+            {
+                lock (_lockObject)
+                {
+                    if (_customerManager == null) //fieldimiz daha önce çağrılmış mı kontrol ediyoruz.
+                    {
+                        _customerManager = new CustomerManager();//çağrılmamış ise new ataması yapıyoruz.
+                    }
+                }
+                
+            }
+            return _customerManager;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Customer Saved!");
+        }
+    }
+
+    class DataAccess
+    {
+        private static DataAccess _dataAccess;
+        private static object _objectLock = new object();
+        private DataAccess()
+        {
+        }
+        public static DataAccess CreateInstance()
+        {
+            if (_dataAccess==null)
+            {
+                lock (_objectLock)
+                {
+                    if (_dataAccess == null)
+                    {
+                        _dataAccess = new DataAccess();
+
+                    }
+                }      
+            }
+            return _dataAccess;
+        }
+        public void Display()
+        {
+            Console.WriteLine("DataAcccess!");
+        }
+    }
+}
